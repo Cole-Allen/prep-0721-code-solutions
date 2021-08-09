12eac50e5403d69e2b7ad6d9100a9052254b2d7d
Cole Allen <coleallen3333@gmail.com>
2021-08-09 13:40:10 -0700

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,18 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'];
+
+var firstElement = languages[0];
+var thirdElement = languages[2];
+var length = languages.length;
+var lastElement = languages[length - 1];
+
+console.log(languages);
+console.log('The first element of the array is ' + firstElement);
+console.log('The third element of the array is ' + thirdElement);
+console.log('The length of the array is ' + length);
+console.log('The last element of the array is ' + lastElement);
